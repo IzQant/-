3a38c3cba9f8b24a09b6f952b379d49252e8b27a
itsmeYanjya <gunpla592@gmail.com>
2024-01-14 14:33:40 +0900

    Update 색종이 만들기.py

--- 색종이 만들기.py
+++ 색종이 만들기.py
@@ -1,8 +1,30 @@
+import sys
 input = __import__('sys').stdin.readline
+sys.setrecursionlimit(10**7)
 #분할 정복
-N = int(input())
-board = [ [0 for _ in range(N)] for _ in range(N)]
+N = int(input()) #한 변의 길이
+#정사각형이므로 행, 열 둘다 N
+board = [list(map(int, input().split())) for _ in range(N)]
+cw = 0
+cb = 0
+def solve(x, y, N):
+    global cw, cb
+    cp = 0
+    for i in range(x, x + N):
+        for j in range(y, y + N):
+            if board[i][j] == 1:
+                cp += 1
+    if cp == 0:
+        cw += 1
+    elif cp == N**2:
+        cb += 1
+    else:
+        solve(x, y, N//2)
+        solve(x, y + N//2, N//2)
+        solve(x + N//2, y, N//2)
+        solve(x + N//2, y + N//2, N//2)
+    return
 
-def solve(N):
-    
-print(board)
+solve(0, 0, N)
+print(cw)
+print(cb)
